ab95bd94867f85ac5656431d1382a43c8ee35d5b
Natalia González Álvarez <nagonal2004@gmail.com>
2024-11-06 17:26:57 +0100

    eliminada excepcion

--- demoFunkos/src/test/java/org/example/demofunkos/storage/controllers/StorageControllerTest.java
+++ demoFunkos/src/test/java/org/example/demofunkos/storage/controllers/StorageControllerTest.java
@@ -1,6 +1,8 @@
 package org.example.demofunkos.storage.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.example.demofunkos.storage.exceptions.StorageBadRequest;
+import org.example.demofunkos.storage.exceptions.StorageException;
 import org.example.demofunkos.storage.services.StorageService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +20,7 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -50,18 +53,28 @@ class StorageControllerTest {
     @BeforeEach
     void setUp() throws IOException {
         MockitoAnnotations.openMocks(this);
-        Path source = Paths.get("test_imgs/test-image.png");
-        Path destination = Paths.get("imgs/test-image.png");
+
+        Path sourcePath = Paths.get("test_imgs", "test-image20.png");
+        if (Files.notExists(sourcePath)) {
+            Files.createDirectories(sourcePath.getParent());
+            Files.createFile(sourcePath);
+        }
+
+        Path destination = Paths.get("imgs/test-image20.png");
         Files.createDirectories(destination.getParent());
-        Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+
+        Files.copy(sourcePath, destination, StandardCopyOption.REPLACE_EXISTING);
     }
+
 
     @Test
     void serveFile() throws Exception {
-        String filename = "test-image.png";
+        String filename = "test-image20.png";
 
+        Resource resource = mock(Resource.class);
+        File testFile = Files.createFile(Path.of("test_imgs/test-image20.png")).toFile();
         when(storageService.loadAsResource(filename)).thenReturn(resource);
-        when(resource.getFile()).thenReturn(new File("imgs/test-image.png"));
+        when(resource.getInputStream()).thenReturn(new FileInputStream(testFile));
 
         MockHttpServletResponse response = mvc.perform(
                         get(endpoint + "/" + filename)
@@ -75,32 +88,10 @@ class StorageControllerTest {
 
         verify(storageService, times(1)).loadAsResource(filename);
     }
-
-    @Test
-    void serveFileWithoutMimeTypeCheck() throws Exception {
-        String filename = "test-image.png";
-        String filePath = "test_imgs/test-image.png";
-
-        when(storageService.loadAsResource(filename)).thenReturn(resource);
-        when(resource.getFile()).thenReturn(new File(filePath));
-
-        MockHttpServletResponse response = mvc.perform(
-                        get(endpoint + "/" + filename)
-                                .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
-        assertAll(
-                () -> assertEquals(HttpStatus.OK.value(), response.getStatus()),
-                () -> assertTrue(response.getContentLength() > 0)
-        );
-
-        verify(storageService, times(1)).loadAsResource(filename);
-    }
-
 
     @Test
     void serveFileThrowsException() throws Exception {
-        String filename = "test-image.png";
+        String filename = "test-image20.kei";
 
         when(storageService.loadAsResource(filename)).thenReturn(resource);
         when(resource.getFile()).thenThrow(new IOException());
