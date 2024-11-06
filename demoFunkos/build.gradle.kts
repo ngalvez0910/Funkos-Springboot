4b617679b02969b825df77994d95f3da1a7ea5d1
Natalia González Álvarez <nagonal2004@gmail.com>
2024-11-06 13:16:36 +0100

    añadido jacoco

--- demoFunkos/build.gradle.kts
+++ demoFunkos/build.gradle.kts
@@ -40,3 +40,16 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 	finalizedBy("jacocoTestReport")
 }
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required.set(false)
+		csv.required.set(false)
+		html.required.set(true)
+	}
+}
+
+tasks.check {
+	dependsOn(tasks.jacocoTestReport)
+}
